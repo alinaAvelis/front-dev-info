cc76ce86cb64b50d7ca1603fd2cf5f5b363e20a5
ALINA <78532001+alinaAvelis@users.noreply.github.com>
2023-07-08 22:26:00 +0300

    update

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -53,7 +53,7 @@ const MyApp = ({ categories, Component, ...rest }) => {
 
 			<AppHeader categories={categories} />
 			<Component {...pageProps} />
-{/* 
+			{/* 
 			<div id="yandex_rtb_R-A-2501461-2"></div>
 			<Script id="yandex-ads-2">
 				{`
