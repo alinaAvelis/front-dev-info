5721efb0ec890476a64cff1ba79a447211cd2e60
ALINA <78532001+alinaAvelis@users.noreply.github.com>
2023-07-10 20:31:49 +0300

    Update

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -53,6 +53,18 @@ const MyApp = ({ categories, Component, ...rest }) => {
 
 			<AppHeader categories={categories} />
 			<Component {...pageProps} />
+
+			<div id="yandex_rtb_R-A-2501461-2"></div>
+			<Script id="yandex-ads-2">
+				{`
+								window.yaContextCb.push(()=>{
+									Ya.Context.AdvManager.render({
+										"blockId": "R-A-2501461-2",
+										"renderTo": "yandex_rtb_R-A-2501461-2"
+									})
+								})
+							`}
+			</Script>
 		</Provider>
 	);
 };
--- src/pages/categories/[slug].tsx
+++ src/pages/categories/[slug].tsx
@@ -70,18 +70,7 @@ const Category = ({pageData, category}) => {
 					</div>
 				</section>
 			</div>
-            
-            <div id="yandex_rtb_R-A-2501461-2"></div>
-			<Script id="yandex-ads-2">
-				{`
-								window.yaContextCb.push(()=>{
-									Ya.Context.AdvManager.render({
-										"blockId": "R-A-2501461-2",
-										"renderTo": "yandex_rtb_R-A-2501461-2"
-									})
-								})
-							`}
-			</Script>
+
         </>
     )
 }
