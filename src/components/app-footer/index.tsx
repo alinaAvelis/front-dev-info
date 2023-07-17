b14d16ce5dfe4c652fa40cdcb5665c591c98a6ed
ALINA <kitegget@gmail.com>
2023-07-17 17:56:03 +0300

    Update

--- src/components/app-footer/index.tsx
+++ src/components/app-footer/index.tsx
@@ -1,22 +1,22 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-
-type AppFooterProps = {
-
-  };
+type AppFooterProps = {};
 
 const AppFooter = ({}: AppFooterProps) => {
-   
-    return (
-        <footer className="footer">
-          <p>© 2022 
-            <a href="https://alina-avelis.com/" target="_blank" rel="noreferrer">
-              Vasileva Alina
-            </a>
-          </p>
-        </footer>
-    )
-}
-
+	return (
+		<footer className="footer">
+			<p>
+				© 2022
+				<a
+					href="https://alina-avelis.com/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Vasileva Alina
+				</a>
+			</p>
+		</footer>
+	);
+};
 
 export default AppFooter;
