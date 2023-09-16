c0d3a299f2700ae19e829bd421a9bb69a279213f
ALINA <78532001+alinaAvelis@users.noreply.github.com>
2023-09-16 17:48:25 +0300

    Update resourses

--- src/pages/resourses/index.jsx
+++ src/pages/resourses/index.jsx
@@ -6,6 +6,7 @@ import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import sanityClient from "../../../public/support-func/sanityClient";
 import MainLayout from "../../layouts/main-layout";
 
 export async function getStaticProps() {
