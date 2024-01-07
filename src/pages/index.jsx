ebf727b1e708a8cda7db22364baa50afc40cc7ff
ALINA <78532001+alinaAvelis@users.noreply.github.com>
2024-01-07 16:56:35 +0300

    Update adds

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
-import Link from "next/link";
 import Script from "next/script";
+import Link from "next/link";
+
 import sanityClient from "../../public/support-func/sanityClient";
 import { sortByDate } from "../../public/support-func/support.js";
 import { setCategoriesState } from "../store/slices/categoriesSlice";
@@ -57,55 +58,7 @@ const HomePage = ({ pageData, categories }) => {
 				<section className="section tabs container  container--center main_container">
 					<h2 className="visually-hidden">Карточки</h2>
 					<div className="tabs_btns flex ">
-						<div className="banner">
-							<div id="yandex_rtb_R-A-2501461-3"></div>
-							<Script
-								id="yandex-ads-3"
-								strategy="afterInteractive"
-							>
-								{`
-									window.yaContextCb.push(()=>{
-										Ya.Context.AdvManager.render({
-											"blockId": "R-A-2501461-3",
-											"renderTo": "yandex_rtb_R-A-2501461-3"
-										})
-									})
-								`}
-							</Script>
-						</div>
-						<Cards data={sortByDate(pageData).slice(0, 3)} />
-						<div className="banner">
-							<div id="yandex_rtb_R-A-2501461-6"></div>
-							<Script
-								id="yandex-ads-6"
-								strategy="afterInteractive"
-							>
-								{`
-										window.yaContextCb.push(()=>{
-											Ya.Context.AdvManager.render({
-												"blockId": "R-A-2501461-6",
-												"renderTo": "yandex_rtb_R-A-2501461-6"
-											})
-										})`}
-							</Script>
-						</div>
-						<Cards data={sortByDate(pageData).slice(3, 6)} />
-						<div className="banner">
-							<div id="yandex_rtb_R-A-2501461-7"></div>
-							<Script
-								id="yandex-ads-7"
-								strategy="afterInteractive"
-							>
-								{`
-										window.yaContextCb.push(()=>{
-											Ya.Context.AdvManager.render({
-												"blockId": "R-A-2501461-7",
-												"renderTo": "yandex_rtb_R-A-2501461-7"
-											})
-										})`}
-							</Script>
-						</div>
-						<Cards data={sortByDate(pageData).slice(6, 9)} />
+						<Cards data={sortByDate(pageData).slice(0, 9)} />
 					</div>
 					{pageData.length > 9 && (
 						<Link
@@ -116,6 +69,112 @@ const HomePage = ({ pageData, categories }) => {
 						</Link>
 					)}
 				</section>
+
+				<div className="aside">
+							{menu.length > 0 && innerWidth > 1200 && (
+								<div
+									className={`menu ${
+										changeMenuPosition && "menu--top"
+									}`}
+								>
+									<h2>Содержание</h2>
+									{menu?.map((item, i) => (
+										<a
+											key={i}
+											className={`menu__item ${item.classList}`}
+											href={`#${item.linkName}`}
+										>
+											{item.text}
+										</a>
+									))}
+								</div>
+							)}
+
+							{innerWidth > 1000 && (
+								<>
+									<div className="banner">
+										<div id="yandex_rtb_R-A-2501461-3"></div>
+										<Script
+											id="yandex-ads-3"
+											strategy="afterInteractive"
+										>
+											{`
+									window.yaContextCb.push(()=>{
+										Ya.Context.AdvManager.render({
+											"blockId": "R-A-2501461-3",
+											"renderTo": "yandex_rtb_R-A-2501461-3"
+										})
+									})
+								`}
+										</Script>
+									</div>
+
+									<div className="banner">
+										<div id="yandex_rtb_R-A-2501461-6"></div>
+										<Script
+											id="yandex-ads-6"
+											strategy="afterInteractive"
+										>
+											{`
+										window.yaContextCb.push(()=>{
+											Ya.Context.AdvManager.render({
+												"blockId": "R-A-2501461-6",
+												"renderTo": "yandex_rtb_R-A-2501461-6"
+											})
+										})`}
+										</Script>
+									</div>
+
+									<div className="banner">
+										<div id="yandex_rtb_R-A-2501461-7"></div>
+										<Script
+											id="yandex-ads-7"
+											strategy="afterInteractive"
+										>
+											{`
+										window.yaContextCb.push(()=>{
+											Ya.Context.AdvManager.render({
+												"blockId": "R-A-2501461-7",
+												"renderTo": "yandex_rtb_R-A-2501461-7"
+											})
+										})`}
+										</Script>
+									</div>
+
+									<div className="banner">
+										<div id="yandex_rtb_R-A-2501461-8"></div>
+										<Script
+											id="yandex-ads-8"
+											strategy="afterInteractive"
+										>
+											{`
+										window.yaContextCb.push(()=>{
+											Ya.Context.AdvManager.render({
+												"blockId": "R-A-2501461-8",
+												"renderTo": "yandex_rtb_R-A-2501461-8"
+											})
+										})`}
+										</Script>
+									</div>
+
+									<div className="banner">
+										<div id="yandex_rtb_R-A-2501461-9"></div>
+										<Script
+											id="yandex-ads-9"
+											strategy="afterInteractive"
+										>
+											{`
+										window.yaContextCb.push(()=>{
+											Ya.Context.AdvManager.render({
+												"blockId": "R-A-2501461-9",
+												"renderTo": "yandex_rtb_R-A-2501461-9"
+											})
+										})`}
+										</Script>
+									</div>
+								</>
+							)}
+						</div>
 			</MainLayout>
 		</>
 	);
