393d63e6922781b372a20b47e5338f382a0fad1f
Alina Vasileva <kitegget@gmail.com>
2026-06-24 10:21:26 +0300

    Add game dev page

---
+++ src/app-pages/game-dev/index.tsx
@@ -0,0 +1,60 @@
+import ResourseBlock from "@/components/resourse-block";
+import PostLayout from "@/layouts/post-layout";
+
+const GameDevPage = () => {
+    return (
+        <PostLayout pathArr={[{ name: "Game Dev" }]}>
+            {data?.map(({ title, data }) => {
+                return <ResourseBlock key={title} title={title} data={data} />;
+            })}
+        </PostLayout>
+    );
+};
+
+
+
+
+
+const info = [
+    {
+        id: 1 + "info",
+        href: `https://learn.unity.com/`,
+        title: `https://learn.unity.com/`,
+        textBeforeStrong: ``,
+        strongText: `курсы Unity`,
+        textAfterStrong: ``,
+    },
+     {
+        id: 2 + "info",
+        href: `https://docs.unity3d.com/ru/530/Manual/index.html`,
+        title: `https://docs.unity3d.com/ru/530/Manual/index.html`,
+        textBeforeStrong: ``,
+        strongText: `документация Unity`,
+        textAfterStrong: ``,
+    },
+];
+
+
+const data = [
+    {
+        title: `Ресурсы`,
+        data: [
+            {
+                data: info,
+            },
+        ],
+    },
+
+];
+// layout
+
+// {
+// 	id: 23,
+// 	href: ``,
+// 	title: ``,
+// 	textBeforeStrong: ``,
+// 	strongText: ``,
+// 	textAfterStrong: ``,
+// },
+
+export default GameDevPage;
