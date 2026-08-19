09b94a3afd3fee94674f633dffa49cff0d886d05
Alina Vasileva <kitegget@gmail.com>
2026-08-19 11:19:42 +0300

    Fix not found page

--- src/error-handlers/not-found-page.tsx
+++ src/error-handlers/not-found-page.tsx
@@ -3,7 +3,7 @@ import getServerDictionary from "@/dictionary/hooks/get-server-dictionary";
 import ErrorPageLinks from "@/components/error-page-links";
 
 export default async function NotFoundPage() {
-	const text = await getServerDictionary("pageNotFound");
+	const text = await getServerDictionary("page-not-found");
 	return (
 		<div className="post container--center max-w-screen-xl mx-auto main_container relative px-5 md:px-10 mt-40 my-4 text-center flex flex-col items-center justify-center">
 			<h2>404</h2>
